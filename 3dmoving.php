3b911458dcdefe7af65bcb9c1b08978fcd41b7b2
Michel Pereira da Silva <132400123+Michelsiri@users.noreply.github.com>
2023-05-02 23:08:21 -0300

    Add files via upload

---
+++ 3dmoving.php
@@ -0,0 +1,7 @@
+<div class="social-icons">
+  <a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
+  <a href="#" class="instagram"><i class="fab fa-instagram"></i></a>
+  <a href="#" class="youtube"><i class="fab fa-youtube"></i></a>
+</div>
+
+
